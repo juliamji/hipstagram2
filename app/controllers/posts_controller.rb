a6b2fb757dba8fc9b5d24f98dd25343412488c8e
Julia Greenwald <julia.greenwald@gmail.com>
2017-07-14 15:42:08 -0700

    add new post and create new post actions to posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,5 +4,17 @@ class PostsController < ApplicationController
 
   def new
     @post = Post.new
+    redirect_to posts_path
+  end
+
+  def create
+    @post = Post.create(post_params)
+    redirect_to posts_path
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:image, :caption)
   end
 end
