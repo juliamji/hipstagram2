494b13fc5f237204421f21815abb9129c0bb713d
Julia Greenwald <julia.greenwald@gmail.com>
2017-07-24 18:02:25 -0700

    generate migration and create model for adding username to users. modify views to show username. modify routes and create registrations controller to allow for a user-created unique username.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+
+  validates :username, presence: true, length: { minimum: 4, maximum: 16 }
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170724235344) do
+ActiveRecord::Schema.define(version: 20170725000500) do
 
   create_table "posts", force: :cascade do |t|
     t.string "caption"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 20170724235344) do
     t.string "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "username"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
