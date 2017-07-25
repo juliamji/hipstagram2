dc1df5df188bea0f71b5b67650939489976ba100
Julia Greenwald <julia.greenwald@gmail.com>
2017-07-25 15:22:21 -0700

    assign current user id to posts and modify posts controller to incorporate current user into create action

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
 
   validates :username, presence: true, length: { minimum: 4, maximum: 16 }
+  has_many :posts, dependent: :destroy
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
