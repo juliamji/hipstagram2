f0e8c4b73c28d8dff2c0d641605878052e42b55b
Julia Greenwald <julia.greenwald@gmail.com>
2017-07-26 15:43:19 -0700

    generate comments controller, model, and migration. define create action and establish many-to-many relationship.

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,7 @@ class Post < ApplicationRecord
   validates :user_id, presence: true
   validates :image, presence: true
   belongs_to :user
+  has_many :comments, dependent: :destroy
 
   has_attached_file :image, styles: { :medium => '640px' }
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
 
   validates :username, presence: true, length: { minimum: 4, maximum: 16 }
   has_many :posts, dependent: :destroy
+  has_many :comments, dependent: :destroy
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
