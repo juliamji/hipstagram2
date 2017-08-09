c6cf3389774bc127723ed82fbbac5b5955f7a05e
Julia Greenwald <julia.greenwald@gmail.com>
2017-08-09 15:00:20 -0700

    create links to user profile page and individual user profile pages with appropriate controller and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'profiles/show'
+
   devise_for :users, :controllers => { registrations: 'registrations' }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'posts#index'
@@ -6,4 +8,5 @@ Rails.application.routes.draw do
   resources :posts do
     resources :comments
   end
+  get ':user_name', to: 'profiles#show', as: :profile
 end
